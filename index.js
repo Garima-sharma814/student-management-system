96bfc1dc05e4e7a99e05f962ba26bdd1d88f88c9
Garima <sharmagarima814@gmail.com>
2021-09-04 19:49:38 +0530

    add: routes middleware

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-import express from "express";
-import cors from "cors";
-import studentDB from "./database/studentDB";
+const express = require("express");
+const cors = require("cors");
+const studentDB = require("./database/studentDB");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ extended: true }));
+app.use(express.json({ limit: "20mb", extended: true }));
 
 app.use(cors());
+
+// Student Routes
 app.use("/students", require("./routes/studentRoutes"));
 
 // MongoDb connection
 studentDB();
 
 // listening the server
-app.listen(port, () => console.log(`Server id sunning at ${port}`));
+app.listen(port, () => console.log(`Server is running at ${port}`));
