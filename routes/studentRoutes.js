a3ab3418a9036d6200dbb4ac6a610f642079d917
Garima <sharmagarima814@gmail.com>
2021-09-04 19:48:49 +0530

    add: student routes with controllers

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,10 @@
-import express from "express";
+const express = require("express");
+const { getStudents, createStudent } = require("../controllers/studentController");
+
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("This is the home page");
-});
+router.get("/", getStudents );
+router.post("/", createStudent );
 
 module.exports = router;
