767e5f24447c59f0ae1d6138b654fa17dcfe74d5
Garima <sharmagarima814@gmail.com>
2021-09-04 19:49:07 +0530

    add: controllers to student routes

---
+++ controllers/studentController.js
@@ -0,0 +1,24 @@
+const Student= require("../models/studentModel");
+
+const getStudents = async (req, res) => {
+  try {
+    const allStudents = await Student.find();
+    res.status(200).json(allStudents);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+const createStudent = async (req, res) => {
+  const student = req.body;
+  const newStudent = new Student (student)
+  try { 
+    await newStudent.save();
+    res.status(201).json(newStudent);
+  }
+  catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+};
+
+module.exports = { getStudents, createStudent };
