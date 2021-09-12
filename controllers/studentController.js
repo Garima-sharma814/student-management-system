619ef00046965290968d6f555c706ae326f5a03f
Garima <sharmagarima814@gmail.com>
2021-09-12 13:11:04 +0530

    test: testing the student routes

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,9 +1,9 @@
-const Student= require("../models/studentModel");
+const StudentData = require("../models/studentModel");
 
 const getStudents = async (req, res) => {
   // res.send(`<h1>hey this is student page</h1>`);
   try {
-    const allStudents = await Student.find();
+    const allStudents = await StudentData.find();
     res.status(200).json(allStudents);
   } catch (err) {
     res.status(404).json({ message: err.message });
@@ -12,7 +12,7 @@ const getStudents = async (req, res) => {
 
 const createStudent = async (req, res) => {
   const student = req.body;
-  const newStudent = new Student (student)
+  const newStudent = new StudentData (student)
   try { 
     await newStudent.save();
     res.status(201).json(newStudent);
