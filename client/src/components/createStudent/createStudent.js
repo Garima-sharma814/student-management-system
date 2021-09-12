33d6fc18c8a6e667f54f4173f8cb0a73c5805a23
Garima <sharmagarima814@gmail.com>
2021-09-12 13:13:15 +0530

    add: createStudent and showStudent component

---
+++ client/src/components/createStudent/createStudent.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+export default function AddStudent() {
+  const classes = useStyles();
+  const [student, setStudent] = useState({
+    regId: 0,
+    name: "",
+    course: "",
+    section: "",
+  });
+  const createStudent = () => {
+    axios.post("http://localhost:5000/students", student);
+  };
+  return (
+    <>
+      <h1>Add your data</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="standard-basic"
+          label="Reg. Id"
+          value={student.regId}
+          onChange={(event) => {
+            setStudent({ ...student, regId: event.target.value });
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          label="Name"
+          value={student.name}
+          onChange={(event) => {
+            setStudent({ ...student, name: event.target.value });
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          label="Course"
+          value={student.course}
+          onChange={(event) => {
+            setStudent({ ...student, course: event.target.value });
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          label="Section"
+          value={student.section}
+          onChange={(event) => {
+            setStudent({ ...student, section: event.target.value });
+          }}
+        />
+        <Button variant="contained" color="primary" onClick={createStudent}>
+          Submit
+        </Button>
+      </form>
+    </>
+  );
+}
