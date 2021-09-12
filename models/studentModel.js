7464ad11ad199d87480d838a88db1ff403469b74
Garima <sharmagarima814@gmail.com>
2021-09-12 13:10:20 +0530

    remove: subject from the student schema

--- models/studentModel.js
+++ models/studentModel.js
@@ -4,12 +4,10 @@ const studentSchema = new mongoose.Schema({
     regId: Number,
     name: String,
     course: String,
-    address: String,
     section:{
         type: String,
         default: "A"
     },
-    subjects: [String]
 });
 
 const studentModel = mongoose.model("student", studentSchema);
