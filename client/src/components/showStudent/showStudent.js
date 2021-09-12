99e55be900167f04202bc8ad3dda0d5e3d2a5176
Garima <sharmagarima814@gmail.com>
2021-09-12 13:59:49 +0530

    feat: diaplay student data from the DB

--- client/src/components/showStudent/showStudent.js
+++ client/src/components/showStudent/showStudent.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import axios from "axios";
 
 const useStyles = makeStyles({
   table: {
@@ -14,53 +15,43 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function ShowStudent() {
   const classes = useStyles();
-  const [ studentList, showStudentList ] = useState(() => {
-    
-  })
+  const [studentList, showStudentList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/students").then((allStudents) => {
+      showStudentList(allStudents.data);
+    });
+  }, []);
 
   return (
-      <>
-    <h1>All Students</h1>
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <>
+      <h1>All Students</h1>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" fontWeight="bold">RegId</TableCell>
+              <TableCell align="center" fontWeight="bold">Name</TableCell>
+              <TableCell align="center" fontWeight="bold">Course</TableCell>
+              <TableCell align="center" fontWeight="bold">Section</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {studentList.map((student, key) => (
+              <TableRow key={student.key}>
+                <TableCell align="center" component="th" scope="row">
+                  {student.regId}
+                </TableCell>
+                <TableCell align="center">{student.name}</TableCell>
+                <TableCell align="center">{student.course}</TableCell>
+                <TableCell align="center">{student.section}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
