359e9aef84b66e4de44c1cfa8329f90f0022ae0e
Garima <sharmagarima814@gmail.com>
2021-09-12 13:12:00 +0530

    add: styles for the UI

---
+++ client/src/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(()=>({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+}))
