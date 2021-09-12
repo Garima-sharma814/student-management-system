4733e02f4afedc236503426c69ace792226ab6ed
Garima <sharmagarima814@gmail.com>
2021-09-12 14:48:36 +0530

    fix: MongoDB to local

--- database/studentDB.js
+++ database/studentDB.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 
 module.exports = function studentDB() {
-mongoose
-  .connect(process.env.connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+// Connect to MongoDB locally
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
